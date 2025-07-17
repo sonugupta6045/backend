3522668a35ab227600ca773f92c1bd5efe9d37d9
sonu <sonugupta6045@gmail.com>
2025-07-17 21:14:37 +0530

    file  uploading  setup with cloudnary

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
+  cloud_api: process.env.CLOUDNARY_CLOUD_API,
+  cloud_secret: CLOUDNARY_CLOUD_SECRET,
+});
+
+const uploadOnCloudnary  = async(localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+
+        // upload file on cloudnary 
+      const respone =  await  cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("file is uploadate succesfully", respone.url);
+        return respone;
+        
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)   // remove the locally  saved temproray file as the upload  operation got failed
+
+        return null;
+
+    }
+}
+
+export { uploadOnCloudnary}
