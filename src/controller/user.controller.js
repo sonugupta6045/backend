df1628bc813955677041880c6db00745b2fe7926
sonu <sonugupta6045@gmail.com>
2025-07-24 18:53:23 +0530

    add getWatchHistory function to retrieve user's watch history with video and owner details

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -8,6 +8,8 @@ import { uploadOnCloudnary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiRespone.js";
 import jwt from "jsonwebtoken";
 import { subscribe } from "diagnostics_channel";
+import mongoose from "mongoose";
+import { use } from "react";
  
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -419,5 +421,57 @@ const getUserChannelProfile =  asyncHandler(async(req, res)=>{
 
 })
 
+const getWatchHistory = asyncHandler(async(req, res)=>{
+  const user =  await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
 
-export { registerUser, loginUser, logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails,updateUserAvatar, updateUserConverImage, getUserChannelProfile };
+    $lookup:{
+      from : "videos",
+      localField: "watchHistory",
+      foreignField: "_id",
+      as: "watchHistory",
+      pipeline: [
+        {
+          $lookup : {
+            from : "users",
+            localField: "owner",
+            foreignField : "_id",
+            as : owner,
+            pipeline :{
+              $project : {
+                fullname:1,
+                username:1,
+                avatar:1
+
+
+
+              }
+            }
+          }
+        },
+        {
+          $addFields : {
+            owner : {
+              $first : "$owner"
+            }
+          }
+        }
+      ]
+    }
+  }
+  ])
+
+  return res.status(200)
+  .json( new ApiResponse(200, 
+    user[0].watchHistory,
+    " Watch History are Fetched successfully"
+  ))
+})
+
+
+export { registerUser, loginUser, logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails,updateUserAvatar, updateUserConverImage, getUserChannelProfile,getWatchHistory };
